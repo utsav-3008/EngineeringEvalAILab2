9899120f6d389f39e371675b65c1dcef2d55d7fd
Visakh V Nair <visakh417@gmail.com>
2024-06-12 18:54:28 +0100

    Deal with noises in data: Added functionality to remove most frequent and less important words from the data

--- main.py
+++ main.py
@@ -83,3 +83,61 @@ def trans_to_en(texts):
 # Note that the we can only translate a limited number of words so we are only translating ticket summary and not interaction content
 temp = df
 temp["ts_en"] = trans_to_en(temp["ts"].to_list())
+
+noise = "(sv\s*:)|(wg\s*:)|(ynt\s*:)|(fw(d)?\s*:)|(r\s*:)|(re\s*:)|(\[|\])|(aspiegel support issue submit)|(null)|(nan)|((bonus place my )?support.pt 自动回复:)"
+temp["ts"] = temp["Ticket Summary"].str.lower().replace(noise, " ", regex=True).replace(r'\s+', ' ',
+                                                                                        regex=True).str.strip()
+temp_debug = temp.loc[:, ["Ticket Summary", "ts", "y"]]
+
+temp["ic"] = temp["Interaction content"].str.lower()
+noise_1 = [
+    "(from :)|(subject :)|(sent :)|(r\s*:)|(re\s*:)",
+    "(january|february|march|april|may|june|july|august|september|october|november|december)",
+    "(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)",
+    "(monday|tuesday|wednesday|thursday|friday|saturday|sunday)",
+    "\d{2}(:|.)\d{2}",
+    "(xxxxx@xxxx\.com)|(\*{5}\([a-z]+\))",
+    "dear ((customer)|(user))",
+    "dear",
+    "(hello)|(hallo)|(hi )|(hi there)",
+    "good morning",
+    "thank you for your patience ((during (our)? investigation)|(and cooperation))?",
+    "thank you for contacting us",
+    "thank you for your availability",
+    "thank you for providing us this information",
+    "thank you for contacting",
+    "thank you for reaching us (back)?",
+    "thank you for patience",
+    "thank you for (your)? reply",
+    "thank you for (your)? response",
+    "thank you for (your)? cooperation",
+    "thank you for providing us with more information",
+    "thank you very kindly",
+    "thank you( very much)?",
+    "i would like to follow up on the case you raised on the date",
+    "i will do my very best to assist you"
+    "in order to give you the best solution",
+    "could you please clarify your request with following information:"
+    "in this matter",
+    "we hope you(( are)|('re)) doing ((fine)|(well))",
+    "i would like to follow up on the case you raised on",
+    "we apologize for the inconvenience",
+    "sent from my huawei (cell )?phone",
+    "original message",
+    "customer support team",
+    "(aspiegel )?se is a company incorporated under the laws of ireland with its headquarters in dublin, ireland.",
+    "(aspiegel )?se is the provider of huawei mobile services to huawei and honor device owners in",
+    "canada, australia, new zealand and other countries",
+    "\d+",
+    "[^0-9a-zA-Z]+",
+    "(\s|^).(\s|$)"]
+for noise in noise_1:
+    print(noise)
+    temp["ic"] = temp["ic"].replace(noise, " ", regex=True)
+temp["ic"] = temp["ic"].replace(r'\s+', ' ', regex=True).str.strip()
+temp_debug = temp.loc[:, ["Interaction content", "ic", "y"]]
+
+print(temp.y1.value_counts())
+good_y1 = temp.y1.value_counts()[temp.y1.value_counts() > 10].index
+temp = temp.loc[temp.y1.isin(good_y1)]
+print(temp.shape)
